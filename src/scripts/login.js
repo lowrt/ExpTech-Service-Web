ca8854f2c25fdf01d129bb8d269609f297f0c776
夜夜 <vcde0000@gmail.com>
2024-03-30 11:20:28 +0800

    fix: user.js

--- src/scripts/login.js
+++ src/scripts/login.js
@@ -39,7 +39,7 @@ submit.addEventListener("click", (e) => {
   })
     .then(async res => {
       if (res.ok) {
-        window.location.href = `./user.html?key=${await res.text()}`;
+        window.location.href = `./user.html?key=${await res.text()}&email=${values.email}`;
       } else {
         switch (await res.text()) {
           case "Invaild email!": {
